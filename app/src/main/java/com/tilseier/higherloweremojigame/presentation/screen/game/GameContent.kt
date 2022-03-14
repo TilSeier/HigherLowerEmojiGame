a12ee66560c17698d9e6775d25ed082c7615e33a
Michael Vaskiv <michael.vaskiv.dev@gmail.com>
2022-03-14 12:57:18 +0200

    Game screen swap items logic

--- app/src/main/java/com/tilseier/higherloweremojigame/presentation/screen/game/GameContent.kt
+++ app/src/main/java/com/tilseier/higherloweremojigame/presentation/screen/game/GameContent.kt
@@ -1,24 +1,127 @@
 package com.tilseier.higherloweremojigame.presentation.screen.game
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.LazyListState
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import com.tilseier.higherloweremojigame.model.Item
+import dev.chrisbanes.snapper.ExperimentalSnapperApi
 import com.tilseier.higherloweremojigame.ui.theme.HigherLowerEmojiGameTheme
+import dev.chrisbanes.snapper.SnapOffsets
+import dev.chrisbanes.snapper.rememberSnapperFlingBehavior
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
+@OptIn(ExperimentalSnapperApi::class)
 @Composable
-fun GameContent() { // movie: Movie
-    Text(text = "GameContent")
+fun GameContent() {
+
+    var currentItems by remember {
+        mutableStateOf(
+            listOf(
+                Item(
+                    name = "Emoji 1",
+                    imageUrl = "",
+                    number = 200
+                ),
+                Item(
+                    name = "Emoji 2",
+                    imageUrl = "",
+                    number = 300
+                ),
+                Item(
+                    name = "Emoji 3",
+                    imageUrl = "",
+                    number = 400
+                )
+            )
+        )
+    }
+
+    Column {
+
+        val lazyListState: LazyListState = rememberLazyListState()
+        val coroutineScope = rememberCoroutineScope()
+
+        // TODO move logic to ViewModel
+        // TODO design of game screen
+        // TODO disable user scroll interaction
+        SideEffect {
+            coroutineScope.launch {
+                delay(2000)
+                lazyListState.animateScrollToItem(1)
+                lazyListState.scrollToItem(0)
+                currentItems = listOf(
+                    Item(
+                        name = "Emoji 2",
+                        imageUrl = "",
+                        number = 300
+                    ),
+                    Item(
+                        name = "Emoji 3",
+                        imageUrl = "",
+                        number = 400
+                    ),
+                    Item(
+                        name = "Emoji 4",
+                        imageUrl = "",
+                        number = 500
+                    )
+                )
+
+                delay(2000)
+                lazyListState.animateScrollToItem(1)
+                lazyListState.scrollToItem(0)
+                currentItems = listOf(
+                    Item(
+                        name = "Emoji 3",
+                        imageUrl = "",
+                        number = 400
+                    ),
+                    Item(
+                        name = "Emoji 4",
+                        imageUrl = "",
+                        number = 500
+                    ),
+                    Item(
+                        name = "Emoji 5",
+                        imageUrl = "",
+                        number = 600
+                    ),
+                )
+                // TODO on animation scroll end
+            }
+        }
+
+        LazyColumn(
+            modifier = Modifier.fillMaxSize(),
+            state = lazyListState,
+            flingBehavior = rememberSnapperFlingBehavior(
+                lazyListState = lazyListState,
+                snapOffsetForItem = SnapOffsets.Start
+            ),
+        ) {
+            items(currentItems) { item ->
+                Text(
+                    text = "Page: ${item.name} = ${item.number}",
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .fillParentMaxHeight(fraction = 0.5f)
+                        .background(color = Color.Blue)
+                )
+            }
+        }
+    }
 
 
-    // Display 10 items
-//    VerticalPager(count = 10) { page ->
-//        // Our page content
-//        Text(
-//            text = "Page: $page",
-//            modifier = Modifier.fillMaxWidth()
-//        )
-//    }
-
+    // TODO remove
 //    val scrollState = rememberScrollState()
 //    Card(
 //        elevation = 0.dp,
