b7a31808fb758e98b20c706e27e7dc85603654d2
Michael Vaskiv <michael.vaskiv.dev@gmail.com>
2022-03-15 13:04:09 +0200

    Swap image and sign emoji items

--- app/src/main/java/com/tilseier/higherloweremojigame/model/Item.kt
+++ app/src/main/java/com/tilseier/higherloweremojigame/model/Item.kt
@@ -1,7 +1,12 @@
 package com.tilseier.higherloweremojigame.model
 
+import androidx.compose.ui.graphics.Color
+
 data class Item(
+    val id: Long,
     val name: String,
-    val imageUrl: String,
+    val imageUrl: String, // TODO maybe list of images
+    val sign: String? = null,
+    val backgroundColor: Color,
     val number: Long
 )
--- app/src/main/java/com/tilseier/higherloweremojigame/presentation/screen/game/GameContent.kt
+++ app/src/main/java/com/tilseier/higherloweremojigame/presentation/screen/game/GameContent.kt
@@ -1,16 +1,17 @@
 package com.tilseier.higherloweremojigame.presentation.screen.game
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.LazyListState
-import androidx.compose.foundation.lazy.items
-import androidx.compose.foundation.lazy.rememberLazyListState
+import androidx.compose.foundation.lazy.*
 import androidx.compose.material.Text
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import coil.compose.rememberImagePainter
 import com.tilseier.higherloweremojigame.model.Item
 import dev.chrisbanes.snapper.ExperimentalSnapperApi
 import com.tilseier.higherloweremojigame.ui.theme.HigherLowerEmojiGameTheme
@@ -23,25 +24,61 @@ import kotlinx.coroutines.launch
 @Composable
 fun GameContent() {
 
-    var currentItems by remember {
-        mutableStateOf(
-            listOf(
-                Item(
-                    name = "Emoji 1",
-                    imageUrl = "",
-                    number = 200
-                ),
-                Item(
-                    name = "Emoji 2",
-                    imageUrl = "",
-                    number = 300
-                ),
-                Item(
-                    name = "Emoji 3",
-                    imageUrl = "",
-                    number = 400
-                )
-            )
+    val faceWithTearsOfJoy = Item(
+        id = 1,
+        name = "Face with Tears of Joy",
+        imageUrl = "https://emojipedia-us.s3.dualstack.us-west-1.amazonaws.com/thumbs/240/apple/325/face-with-tears-of-joy_1f602.png",
+        sign = "\uD83D\uDE02", // 😂
+        backgroundColor = Color.White,
+        number = 200
+    )
+    val redHeart = Item(
+        id = 2,
+        name = "Red Heart",
+        imageUrl = "https://emojipedia-us.s3.dualstack.us-west-1.amazonaws.com/thumbs/240/apple/325/red-heart_2764-fe0f.png",
+        sign = "❤️️",
+        backgroundColor = Color.LightGray,
+        number = 300
+    )
+    val faceBlowingKiss = Item(
+        id = 3,
+        name = "Face Blowing a Kiss",
+        imageUrl = "https://emojipedia-us.s3.dualstack.us-west-1.amazonaws.com/thumbs/240/apple/325/face-blowing-a-kiss_1f618.png",
+        sign = "\uD83D\uDE18", // 😘
+        backgroundColor = Color.Green,
+        number = 400
+    )
+    val smilingFaceWithHeartEyes = Item(
+        id = 4,
+        name = "Smiling Face with Heart-Eyes",
+        imageUrl = "https://emojipedia-us.s3.dualstack.us-west-1.amazonaws.com/thumbs/240/apple/325/smiling-face-with-heart-eyes_1f60d.png",
+        sign = "\uD83D\uDE0D", // 😍
+        backgroundColor = Color.Cyan,
+        number = 500
+    )
+    val rollingOnTheFloorLaughingFace = Item(
+        id = 5,
+        name = "Rolling on the Floor Laughing Face",
+        imageUrl = "https://emojipedia-us.s3.dualstack.us-west-1.amazonaws.com/thumbs/240/apple/325/rolling-on-the-floor-laughing_1f923.png",
+        sign = "\uD83E\uDD23", // 🤣
+        backgroundColor = Color.Blue,
+        number = 600
+    )
+
+//    var currentItems by remember {
+//        mutableStateOf(
+//            listOf(
+//                faceWithTearsOfJoy,
+//                redHeart,
+//                faceBlowingKiss
+//            )
+//        )
+//    }
+    val currentItems = remember {
+        mutableStateListOf(
+            faceWithTearsOfJoy,
+            redHeart,
+            faceBlowingKiss
         )
     }
 
@@ -56,47 +93,29 @@ fun GameContent() {
         SideEffect {
             coroutineScope.launch {
                 delay(2000)
+                currentItems.add(smilingFaceWithHeartEyes)
                 lazyListState.animateScrollToItem(1)
-                lazyListState.scrollToItem(0)
-                currentItems = listOf(
-                    Item(
-                        name = "Emoji 2",
-                        imageUrl = "",
-                        number = 300
-                    ),
-                    Item(
-                        name = "Emoji 3",
-                        imageUrl = "",
-                        number = 400
-                    ),
-                    Item(
-                        name = "Emoji 4",
-                        imageUrl = "",
-                        number = 500
-                    )
-                )
+//                lazyListState.scrollToItem(0)
+//                currentItems.remove(faceWithTearsOfJoy)
+//                currentItems.add(smilingFaceWithHeartEyes)
+//                currentItems = listOf(
+//                    redHeart,
+//                    faceBlowingKiss,
+//                    smilingFaceWithHeartEyes
+//                )
 
                 delay(2000)
-                lazyListState.animateScrollToItem(1)
-                lazyListState.scrollToItem(0)
-                currentItems = listOf(
-                    Item(
-                        name = "Emoji 3",
-                        imageUrl = "",
-                        number = 400
-                    ),
-                    Item(
-                        name = "Emoji 4",
-                        imageUrl = "",
-                        number = 500
-                    ),
-                    Item(
-                        name = "Emoji 5",
-                        imageUrl = "",
-                        number = 600
-                    ),
-                )
-                // TODO on animation scroll end
+                currentItems.add(rollingOnTheFloorLaughingFace)
+                lazyListState.animateScrollToItem(2)
+//                lazyListState.scrollToItem(0)
+//                currentItems.remove(redHeart)
+//                currentItems.add(rollingOnTheFloorLaughingFace)
+
+//                currentItems = listOf(
+//                    faceBlowingKiss,
+//                    smilingFaceWithHeartEyes,
+//                    rollingOnTheFloorLaughingFace,
+//                )
             }
         }
 
@@ -108,67 +127,63 @@ fun GameContent() {
                 snapOffsetForItem = SnapOffsets.Start
             ),
         ) {
-            items(currentItems) { item ->
-                Text(
-                    text = "Page: ${item.name} = ${item.number}",
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .fillParentMaxHeight(fraction = 0.5f)
-                        .background(color = Color.Blue)
-                )
+            itemsIndexed(currentItems) { index, item ->
+                // TODO first item always opened
+                if (item.sign == null) {
+                    ItemWithEmojiImage(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .fillParentMaxHeight(fraction = 0.5f)
+                            .background(color = item.backgroundColor),
+                        item = item
+                    )
+                } else {
+                    ItemWithEmojiSign(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .fillParentMaxHeight(fraction = 0.5f)
+                            .background(color = item.backgroundColor),
+                        item = item
+                    )
+                }
             }
         }
     }
+}
 
+@Composable
+private fun ItemWithEmojiImage(
+    modifier: Modifier = Modifier,
+    item: Item
+) {
+    Box(modifier = modifier) {
+        val emojiImage = rememberImagePainter(item.imageUrl)
+        Image(
+            painter = emojiImage,
+            contentDescription = null,
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(30.dp)
+        )
+    }
+}
 
-    // TODO remove
-//    val scrollState = rememberScrollState()
-//    Card(
-//        elevation = 0.dp,
-//        backgroundColor = MaterialTheme.colors.AppThemeColor
-//    ) {
-//        Column(
-//            modifier = Modifier
-//                .fillMaxWidth()
-//                .verticalScroll(scrollState)
-//        ) {
-//            Image(
-//                painter = rememberImagePainter(
-//                    data = BuildConfig.POSTER_URL + movie.posterPath, builder = {
-//                        crossfade(true)
-//                        scale(Scale.FIT)
-//                    }),
-//                contentDescription = null,
-//                modifier = Modifier
-//                    .fillMaxWidth()
-//                    .height(350.dp),
-//                contentScale = ContentScale.FillWidth
-//            )
-//            Column(modifier = Modifier.padding(8.dp)) {
-//                Spacer(modifier = Modifier.height(16.dp))
-//                movie.title?.let {
-//                    Text(
-//                        text = it,
-//                        style = MaterialTheme.typography.h5,
-//                        fontWeight = FontWeight.Bold
-//                    )
-//                }
-//                Spacer(modifier = Modifier.height(8.dp))
-//                movie.releaseDate?.let {
-//                    ReleaseDateComponent(releaseDate = it)
-//                }
-//                Spacer(modifier = Modifier.height(8.dp))
-//                movie.rating?.let { RatingComponent(rating = it) }
-//                Spacer(modifier = Modifier.height(16.dp))
-//                movie.overview?.let {
-//                    Text(
-//                        text = it,
-//                        style = MaterialTheme.typography.body2
-//                    )
-//                }
-//            }
-//        }
-//    }
+@Composable
+private fun ItemWithEmojiSign(
+    modifier: Modifier = Modifier,
+    item: Item
+) {
+    Box(modifier = modifier) {
+        item.sign?.let { sign ->
+            Text(
+                text = sign,
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(30.dp),
+                fontSize = 120.sp
+            )
+        }
+    }
 }
 
 @Preview(showBackground = true)
